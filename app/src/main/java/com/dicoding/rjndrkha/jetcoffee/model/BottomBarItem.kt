93b8fa9ae75adfc98f7564827aad1834f440d5c1
Rjndrkha <75615789+Rjndrkha@users.noreply.github.com>
2023-04-16 15:52:15 +0700

    Done All Practice

---
+++ app/src/main/java/com/dicoding/rjndrkha/jetcoffee/model/BottomBarItem.kt
@@ -0,0 +1,7 @@
+package com.dicoding.rjndrkha.jetcoffee.model
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomBarItem(val title: String, val icon: ImageVector) {
+
+}
---
+++ app/src/main/java/com/dicoding/rjndrkha/jetcoffee/ui/theme/components/CategoryItem.kt
@@ -0,0 +1,56 @@
+package com.dicoding.rjndrkha.jetcoffee.ui.theme.components
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.dicoding.rjndrkha.jetcoffee.model.Category
+import com.dicoding.rjndrkha.jetcoffee.R
+import com.dicoding.rjndrkha.jetcoffee.ui.theme.JetCoffeeTheme
+
+@Composable
+fun CategoryItem(
+    category: Category,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(category.imageCategory),
+            contentDescription = null,
+            modifier = Modifier
+                .size(60.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(category.textCategory),
+            fontSize = 10.sp,
+            modifier = Modifier.paddingFromBaseline(top = 16.dp, bottom = 8.dp)
+        )
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+fun CategoryItemPreview() {
+    JetCoffeeTheme {
+        CategoryItem(
+            category = Category(
+                R.drawable.icon_category_cappuccino,
+                R.string.category_cappuccino,
+            ),
+            modifier = Modifier.padding(horizontal = 8.dp)
+        )
+    }
+}
